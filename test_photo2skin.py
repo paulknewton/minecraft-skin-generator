c9345f95a0332c9304cc1983f1935c44b33762c0
Paul Newton <paul@topolino.org>
2019-04-18 21:59:43 +0200

    Updated unit tests

--- test_photo2skin.py
+++ test_photo2skin.py
@@ -1,5 +1,6 @@
 import pytest
 import photo2skin
+from PIL import Image, ImageChops
 
 
 def test_it():
@@ -14,3 +15,17 @@ def test_missing_file():
 def test_existing_file():
     photo2skin.build_skin("test.jpg", 0, 0)
     assert True
+
+
+def test_skin():
+    new_skin = photo2skin.build_skin("test.jpg", 6, 5)
+    test_skin = Image.open("test-skin.png")
+
+    assert ImageChops.difference(new_skin, test_skin).getbbox() is None
+
+
+def test_thumbnail():
+    new_thumb = photo2skin.build_thumbnail(photo2skin.build_skin("test.jpg", 6, 5), 6, 5)
+    test_thumb = Image.open("test-thumb.png")
+
+    assert ImageChops.difference(new_thumb, test_thumb).getbbox() is None
